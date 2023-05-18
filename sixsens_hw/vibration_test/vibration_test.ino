52d77f59a561e99db190868b164bd75876d054d8
ThatAquarel <xia_tianyi@outlook.com>
2023-05-18 10:29:53 -0400

    hardware

---
+++ sixsens_hw/vibration_test/vibration_test.ino
@@ -0,0 +1,34 @@
+#define COLS 8
+#define ROWS 6
+
+const int a[3] = {D7, D8, D9};
+const int r[6] = {D1, D2, D3, D4, D5, D6};
+const int enable = D0;
+
+void setup() {
+  for (int i = 0; i < 3; i++) {
+    pinMode(a[i], OUTPUT);
+    digitalWrite(a[i], HIGH);
+  }
+  for (int i = 0; i < ROWS; i++) {
+    pinMode(r[i], OUTPUT);
+    digitalWrite(r[i], HIGH);
+  }
+  
+  pinMode(enable, OUTPUT);
+  digitalWrite(enable, HIGH);
+}
+
+void loop() {
+  for (int col = 0; col < COLS; col++) {
+    for (int j = 0; j < 3; j++){
+      digitalWrite(a[j], (1 << j) & (~col));
+    }
+
+    for (int row = 0; row < ROWS; row++) {
+      digitalWrite(r[row], LOW);
+      delay(50);
+      digitalWrite(r[row], HIGH);
+    }
+  }
+}
